ce5621b5d50e13f18a7524da5a479bc08d94bb92
wwx <593805475@qq.com>
2020-05-29 23:28:06 +0800

    添加主界面选项背景

--- app/src/main/java/com/example/fe/FeLayoutMainTheme.java
+++ app/src/main/java/com/example/fe/FeLayoutMainTheme.java
@@ -2,10 +2,13 @@ package com.example.fe;
 
 import android.content.Context;
 import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.util.DisplayMetrics;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -23,7 +26,7 @@ public class FeLayoutMainTheme extends RelativeLayout {
     private RelativeLayout.LayoutParams tvPressAnyKeyParam = null;
 
     //菜单信息
-    private TextView tvLoad = null, tvNew = null, tvCopy = null, tvDel = null, tvElse = null;
+    private Button tvLoad = null, tvNew = null, tvCopy = null, tvDel = null, tvElse = null;
 
     //菜单线性布局参数
     private LinearLayout linearLayout = null;
@@ -31,45 +34,50 @@ public class FeLayoutMainTheme extends RelativeLayout {
     private LinearLayout.LayoutParams tvLayoutParams = null;
 
     //主界面触屏事件回调函数
+    private View.OnClickListener onClickListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            //点击:加载存档
+            if(v == tvLoad){
+                FeData.flow.loadSection(0);
+            }
+            //点击:创建存档
+            else if(v == tvNew){
+                FeData.flow.loadSection(0);
+            }
+            //点击:复制存档
+            else if(v == tvCopy){
+                ;
+            }
+            //点击:删除存档
+            else if(v == tvDel){
+                ;
+            }
+            //点击:附加内容
+            else if(v == tvElse){
+                ;
+            }
+        }
+    };
+
+    //主界面触屏事件回调函数
     private View.OnTouchListener onTouchListener  = new View.OnTouchListener (){
         public boolean onTouch(View v, MotionEvent event) {
             //触屏UP时
             if(event.getAction() == MotionEvent.ACTION_UP)
             {
-                //点击:加载存档
-                if(v == tvLoad){
-                    FeData.flow.loadSection(0);
-                }
-                //点击:创建存档
-                else if(v == tvNew){
-                    FeData.flow.loadSection(0);
-                }
-                //点击:复制存档
-                else if(v == tvCopy){
-                    ;
-                }
-                //点击:删除存档
-                else if(v == tvDel){
-                    ;
-                }
-                //点击:附加内容
-                else if(v == tvElse){
-                    ;
-                }
                 //点击:空白处
-                else{
-                    //菜单显示 -> 移除菜单
-                    if(showStatus == 0) {
-                        removeTips();
-                        loadMenu();
-                        showStatus = 1;
-                    }
-                    //没有菜单 -> 显示菜单
-                    else if(showStatus == 1){
-                        removeMenu();
-                        loadTip();
-                        showStatus = 0;
-                    }
+                //菜单显示 -> 移除菜单
+                if(showStatus == 0) {
+                    removeTips();
+                    loadMenu();
+                    showStatus = 1;
+                }
+                //没有菜单 -> 显示菜单
+                else if(showStatus == 1){
+                    removeMenu();
+                    loadTip();
+                    showStatus = 0;
                 }
             }
             return true;//不返回true的话ACTION_DOWN之后的事件都会被丢弃
@@ -92,7 +100,7 @@ public class FeLayoutMainTheme extends RelativeLayout {
         //加载按任意键提示
         loadTip();
 
-        this.setBackgroundColor(0xFF808080);
+        this.setBackgroundColor(0xFF408040);
         this.setOnTouchListener(onTouchListener);
     }
 
@@ -106,7 +114,8 @@ public class FeLayoutMainTheme extends RelativeLayout {
             //按任意键继续
             tvPressAnyKey = new TextView(FeData.context);
             tvPressAnyKey.setText("按任意键继续");
-            tvPressAnyKey.setTextSize(16);
+            tvPressAnyKey.setTextColor(0x80FFFFFF);
+            tvPressAnyKey.setTextSize(24);
             //相对主界面的位置
             tvPressAnyKeyParam = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
             tvPressAnyKeyParam.addRule(RelativeLayout.CENTER_HORIZONTAL);
@@ -126,43 +135,37 @@ public class FeLayoutMainTheme extends RelativeLayout {
         this.removeView(tvPressAnyKey);
     }
 
+    private void _setTxetView(Button button, String text){
+        button.setText(text);
+        button.setTextColor(0xFFFFFFFF);
+        button.setTextSize(24);
+//        button.setShadowLayer(5, 5, 5, 0x40404040);
+        button.setGravity(Gravity.CENTER);
+        button.setOnClickListener(onClickListener);
+        button.setBackground(Drawable.createFromStream(getClass().getResourceAsStream("/assets/menu/item/item.png"), null));
+    }
+
     public void loadMenu(){
         //为空则创建
         if(linearLayout == null)
         {
             //菜单各项TXT
-            tvLoad = new TextView(FeData.context);
-            tvLoad.setText("继 续");
-            tvLoad.setTextSize(16);
-            tvLoad.setOnTouchListener(onTouchListener);
-            tvNew = new TextView(FeData.context);
-            tvNew.setText("新游戏");
-            tvNew.setTextSize(16);
-            tvNew.setOnTouchListener(onTouchListener);
-            tvCopy = new TextView(FeData.context);
-            tvCopy.setText("复 制");
-            tvCopy.setTextSize(16);
-            tvCopy.setOnTouchListener(onTouchListener);
-            tvDel = new TextView(FeData.context);
-            tvDel.setText("删 除");
-            tvDel.setTextSize(16);
-            tvDel.setOnTouchListener(onTouchListener);
-            tvElse = new TextView(FeData.context);
-            tvElse.setText("附加内容");
-            tvElse.setTextSize(16);
-            tvElse.setOnTouchListener(onTouchListener);
+            tvLoad = new Button(FeData.context);_setTxetView(tvLoad, "继 续");
+            tvNew = new Button(FeData.context);_setTxetView(tvNew, "新游戏");
+            tvCopy = new Button(FeData.context);_setTxetView(tvCopy, "复 制");
+            tvDel = new Button(FeData.context);_setTxetView(tvDel, "删 除");
+            tvElse = new Button(FeData.context);_setTxetView(tvElse, "附加内容");
             //创建线性布局窗体
             linearLayout = new LinearLayout(FeData.context);
             linearLayout.setOrientation(LinearLayout.VERTICAL);
-            linearLayout.setBackgroundColor(0x80008000);
+//            linearLayout.setBackgroundColor(0x80808080);
             //创建线性布局窗体参数
             tvLayoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
             tvLayoutParams.setMargins(0,0, 0, 10);
             //线性布局窗体相对主界面位置参数
             linearLayoutParam = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
             linearLayoutParam.addRule(RelativeLayout.CENTER_HORIZONTAL);
-            linearLayoutParam.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
-            linearLayoutParam.setMargins(0, 0, 0, 200);
+            linearLayoutParam.addRule(RelativeLayout.CENTER_VERTICAL);
             //添加布局参数
             linearLayout.setLayoutParams(linearLayoutParam);
         }
