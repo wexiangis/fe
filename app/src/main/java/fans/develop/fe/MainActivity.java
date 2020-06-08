faf8d2a1ae96fc2f2f49866f78119b719275fab3
wwx <593805475@qq.com>
2020-06-08 10:09:25 +0800

    添加存储权限检查和申请

--- app/src/main/java/fans/develop/fe/MainActivity.java
+++ app/src/main/java/fans/develop/fe/MainActivity.java
@@ -2,21 +2,36 @@ package fans.develop.fe;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.WindowManager;
 import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         //点击应用图标,回到应用时的特殊处理
         if((getIntent().getFlags() & Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT) != 0) {
             finish();
             return;
         }
+
+        //存储权限检查和申请
+        if (ContextCompat.checkSelfPermission(
+        this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED)
+        {
+            //发起权限申请询问
+            ActivityCompat.requestPermissions(
+        this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+        }
+
         //半透明系统虚拟按键
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
     }
